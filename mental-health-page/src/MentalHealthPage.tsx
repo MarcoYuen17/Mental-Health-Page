b20cc9304970832c121cac2b864dc80945c825cd
Marco Yuen <yuenmarco17@gmail.com>
2021-05-31 21:12:36 -0700

    Smooth scrolling to sections on tab click

--- mental-health-page/src/MentalHealthPage.tsx
+++ mental-health-page/src/MentalHealthPage.tsx
@@ -9,18 +9,23 @@ export type TabType = 'gif' | 'links' | 'journal';
 const MentalHealthPage = () => {
   const [selectedTab, setSelectedTab] = useState<TabType>('gif');
 
+  const handleChangeSelected = (tab: TabType) => {
+    setSelectedTab(tab);
+    document.getElementById(tab)?.scrollIntoView();
+  }
+
   return (
     <div className='mental-health-page'>
       <div className='mental-health-page-header'>
-        <Header activeTab={selectedTab} onChangeSelected={setSelectedTab} />
+        <Header activeTab={selectedTab} onChangeSelected={handleChangeSelected} />
       </div>
-      <div className='mental-health-page-content mental-health-page-content-col1'>
+      <div className='mental-health-page-content mental-health-page-content-col1' id='gif'>
         <BreathingGif />
       </div>
-      <div className='mental-health-page-content mental-health-page-content-col2' style={{gridColumn: 2}}>
+      <div className='mental-health-page-content mental-health-page-content-col2' id='links'>
         <Links />
       </div>
-      <div className='mental-health-page-content mental-health-page-content-col3'>
+      <div className='mental-health-page-content mental-health-page-content-col3' id='journal'>
         Journal
       </div>
     </div>
