8eeeffaf7fcca78e12355726d3ebbf9839fe9cd9
Marco Yuen <yuenmarco17@gmail.com>
2021-05-31 20:01:13 -0700

    Split page into 3 horizontal sections

--- mental-health-page/src/MentalHealthPage.tsx
+++ mental-health-page/src/MentalHealthPage.tsx
@@ -10,17 +10,17 @@ const MentalHealthPage = () => {
   const [selectedTab, setSelectedTab] = useState<TabType>('gif');
 
   return (
-    <div className="App">
-      <div>
+    <div className='mental-health-page'>
+      <div className='mental-health-page-header'>
         <Header activeTab={selectedTab} onChangeSelected={setSelectedTab} />
       </div>
-      <div>
+      <div className='mental-health-page-content mental-health-page-content-col1'>
         <BreathingGif />
       </div>
-      <div>
+      <div className='mental-health-page-content mental-health-page-content-col2' style={{gridColumn: 2}}>
         <Links />
       </div>
-      <div>
+      <div className='mental-health-page-content mental-health-page-content-col3'>
         Journal
       </div>
     </div>
